2adbd678e0cbc4d2203e842acfa9f18a7b096f99
Puvisz Teeweang <59161111@go.buu.ac.th>
2021-03-30 04:27:48 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <MyName name="Puvisz Teeweang"/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MyName from './components/MyName.vue'
+export default {
+  name: 'App',
+  components: {
+    MyName
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
